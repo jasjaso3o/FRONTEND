69300e1f21d9ee27674b7870d156fd239c9fbdf3
jasmin <jasminmamani099@gmail.com>
2025-10-28 20:51:31 -0300

    28/10 intalé tailwind y puse estilos para probar

---
+++ usilly/src/componentes/main.jsx
@@ -0,0 +1,14 @@
+function Feed_principal () {
+  return (
+    <div className="soyElPadre flex flex-row-reverse w-[100vw] h-[100vh]" >
+      <div className="soyElMasChico opacity-0 sm:opacity-100 bg-amber-700 h-50 w-[50%]">
+        a
+      </div>
+      <div className="soyElMayor bg-red-100 h-50 rounded-[50%] w-50 sm:w-50 flex items-center justify-center">
+        b
+      </div>
+    </div>
+  )
+}
+
+export default Feed_principal;
